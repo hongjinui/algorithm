c54f1edaca9bef2ac81470ea3f018993e2261f84
hongjinui <houago3@gmail.com>
2022-02-20 14:33:18 +0900

    good game

---
+++ src/programmers/LottoWinMaxMin.java
@@ -0,0 +1,48 @@
+package programmers;
+
+import java.util.*;
+
+public class LottoWinMaxMin {
+
+    public static void main(String[] args){
+
+        int[] lottos = {44, 1, 0, 0, 31, 25};
+        int[] win_num  = {31, 10, 45, 1, 6, 19};
+
+        int[] result = solution(lottos, win_num);
+
+        for (int i : result) {
+            System.out.print(" " + i);
+        }
+
+    }
+
+    public static int[] solution(int[] lottos, int[] win_nums) {
+
+        Map<Integer, Boolean> hashMap = new HashMap<>();
+
+        int zeroCount = 0;
+        for( int lotto : lottos ){
+            if( lotto == 0 ){
+                zeroCount++;
+                continue;
+            }
+            hashMap.put(lotto, true);
+        }
+
+        int winCount = 0;
+        for( int winNum : win_nums ){
+            if( hashMap.containsKey(winNum) ){
+                winCount++;
+            }
+        }
+
+        int maxRank = 7 - ( zeroCount + winCount );
+        int minRank = 7 - ( winCount );
+        if(maxRank > 6) maxRank = 6;
+        if(minRank > 6) minRank = 6;
+
+        return new int[]{maxRank, minRank};
+
+    }
+}
--- src/programmers/ReturnSingoResult.java
+++ src/programmers/ReturnSingoResult.java
@@ -2,6 +2,11 @@ package programmers;
 
 import java.util.*;
 
+/**
+ * @문제유형 : Map,Set 등 자료구조 문제
+ *
+ */
+
 public class ReturnSingoResult {
 
     public static void main(String[] args) {
@@ -22,94 +27,162 @@ public class ReturnSingoResult {
     }
 
 
+//    public static int[] solution(String[] id_list, String[] report, int k) {
+//
+//        Map<String, Set<String>> badUserMap = new LinkedHashMap<>();
+//        Map<String, Integer> userMap = new LinkedHashMap<>();
+//
+//        for(String user : id_list){
+//            userMap.put(user,0);
+//            badUserMap.put(user, new HashSet<>());
+//        }
+//
+//        for( String rpt : report ){
+//            String[] rptArr = rpt.split(" ");
+//            String user = rptArr[0];
+//            String badUser = rptArr[1];
+//
+//            badUserMap.get(badUser).add(user);
+//        }
+//
+////         for( String rpt : report ){
+////             String[] rptArr = rpt.split(" ");
+////             String user = rptArr[0];
+////             String badUser = rptArr[1];
+//
+////             if( !badUserMap.containsKey(badUser) ){
+//// //                singoCntMap.put(badUser,1);
+////                 badUserMap.put(badUser, new HashSet<>());
+////                 badUserMap.get(badUser).add(user);
+////             }else{
+//// //                singoCntMap.put(badUser, singoCntMap.get(badUser)+1);
+////                 badUserMap.get(badUser).add(user);
+////             }
+////         }
+//
+//        // for( Map.Entry<String,Set<String>> e : badUserMap.entrySet()){
+//        //     if(e.getValue().size() >= 2){
+//        //         // Set<String> set = badUserMap.get(e.getKey());
+//        //         for( String user : e.getValue() ){
+//        //             userMap.put(user, userMap.get(user)+1);
+//        //         }
+//        //     }
+//        // }
+//
+//        for(String badUser : badUserMap.keySet()){
+//            Set<String> userList = badUserMap.get(badUser);
+//            if(userList.size() >= 2){
+//                for( String user : userList ){
+//                    userMap.put(user, userMap.get(user)+1);
+//                }
+//            }
+//        }
+//
+////        for( Map.Entry<String,Integer> entry : singoCntMap.entrySet() ){
+////            if(entry.getValue() >= 2){
+////                Set<String> set = badUserMap.get(entry.getKey());
+////
+////                for( String user : set ){
+////                    userMap.put(user, userMap.get(user)+1);
+////                }
+////            }
+////        }
+//
+//
+//        int[] answer = new int[id_list.length];
+//
+//        for( int i=0; i<id_list.length; i++ ){
+//            answer[i] = userMap.get(id_list[i]);
+//        }
+//
+//        return answer;
+//    }
+//        Map<String, Set<String>> badUserMap = new LinkedHashMap<>();
+//        Map<String, Integer> userMap = new LinkedHashMap<>();
+//
+//        for(String id : id_list){
+//            userMap.put(id,0);
+//        }
+//
+//        for( String rpt : report ){
+//            String[] rptArr = rpt.split(" ");
+//            String user = rptArr[0];
+//            String badUser = rptArr[1];
+//
+//            if( !badUserMap.containsKey(badUser) ){
+//                badUserMap.put(badUser, new HashSet<>());
+//                badUserMap.get(badUser).add(user);
+//            }else{
+//                badUserMap.get(badUser).add(user);
+//            }
+//        }
+//
+//        for( Map.Entry<String,Set<String>> e : badUserMap.entrySet()){
+//            if(e.getValue().size() >= 2){
+//                Set<String> set = badUserMap.get(e.getKey());
+//                for( String user : set ){
+//                    userMap.put(user, userMap.get(user)+1);
+//                }
+//            }
+//        }
+//
+//        int[] answer = new int[id_list.length];
+//
+//        for( int i=0; i<id_list.length; i++ ){
+//            answer[i] = userMap.get(id_list[i]);
+//        }
+//
+//        return answer;
+
+//    }
     public static int[] solution(String[] id_list, String[] report, int k) {
 
-        Map<String, Set<String>> badUserMap = new LinkedHashMap<>();
-        Map<String, Integer> userMap = new LinkedHashMap<>();
+        Set<String> set = new HashSet<>();
+        Map<String, Integer> rptCntMap = new HashMap<>();
+        Map<String, Integer> map = new LinkedHashMap<>();
 
-        for(String id : id_list){
-            userMap.put(id,0);
+        List<Integer> list = new ArrayList<>();
+
+        for (String user : id_list) {
+            map.put(user,0);
         }
 
-        for( String rpt : report ){
+        // 신고 중복 제거
+        for (String rpt : report) {
+            set.add(rpt);
+        }
+
+        // 신고 당한 사람 카운팅
+        for (String rpt : set) {
             String[] rptArr = rpt.split(" ");
             String user = rptArr[0];
             String badUser = rptArr[1];
 
-            if( !badUserMap.containsKey(badUser) ){
-                badUserMap.put(badUser, new HashSet<>());
-                badUserMap.get(badUser).add(user);
-            }else{
-                badUserMap.get(badUser).add(user);
+            if (!rptCntMap.containsKey(badUser)) {
+                rptCntMap.put(badUser, 1);
+            } else {
+                rptCntMap.put(badUser, rptCntMap.get(badUser) + 1);
             }
         }
 
-        for( Map.Entry<String,Set<String>> e : badUserMap.entrySet()){
-            if(e.getValue().size() >= 2){
-                Set<String> set = badUserMap.get(e.getKey());
-                for( String user : set ){
-                    userMap.put(user, userMap.get(user)+1);
-                }
+        for (String rpt : set) {
+            String[] rptArr = rpt.split(" ");
+            String user = rptArr[0];
+            String badUser = rptArr[1];
+
+            if( rptCntMap.get(badUser) >= 2){
+                map.put(user, map.get(user)+1);
             }
         }
-
         int[] answer = new int[id_list.length];
 
-        for( int i=0; i<id_list.length; i++ ){
-            answer[i] = userMap.get(id_list[i]);
+        for(Integer cnt : map.values()){
+            list.add(cnt);
         }
 
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
+        }
         return answer;
-
     }
-//    public static int[] solution(String[] id_list, String[] report, int k) {
-//
-//        Set<String> set = new HashSet<>();
-//        Map<String, Integer> rptCntMap = new HashMap<>();
-//        Map<String, Integer> map = new LinkedHashMap<>();
-//
-//        List<Integer> list = new ArrayList<>();
-//
-//        for (String user : id_list) {
-//            map.put(user,0);
-//        }
-//
-//        // 신고 중복 제거
-//        for (String rpt : report) {
-//            set.add(rpt);
-//        }
-//
-//        // 신고 당한 사람 카운팅
-//        for (String rpt : set) {
-//            String[] rptArr = rpt.split(" ");
-//            String user = rptArr[0];
-//            String badUser = rptArr[1];
-//
-//            if (!rptCntMap.containsKey(badUser)) {
-//                rptCntMap.put(badUser, 1);
-//            } else {
-//                rptCntMap.put(badUser, rptCntMap.get(badUser) + 1);
-//            }
-//        }
-//
-//        for (String rpt : set) {
-//            String[] rptArr = rpt.split(" ");
-//            String user = rptArr[0];
-//            String badUser = rptArr[1];
-//
-//            if( rptCntMap.get(badUser) >= 2){
-//                map.put(user, map.get(user)+1);
-//            }
-//        }
-//        int[] answer = new int[id_list.length];
-//
-//        for(Integer cnt : map.values()){
-//            list.add(cnt);
-//        }
-//
-//        for(int i=0; i<answer.length; i++){
-//            answer[i] = list.get(i);
-//        }
-//        return answer;
-//    }
 }
